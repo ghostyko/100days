e0657de632a59bc549c385a1c637648827355448
dakota <dakotaabarnes@gmail.com>
2024-10-21 21:22:02 -0500

    Heads or Tails

--- Random.py
+++ Random.py
@@ -1,7 +1,19 @@
 import random
-import my_module
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+heads_tails = random.uniform(1, 10)
+print(heads_tails)
 
-print(my_module.favorite_number)
+if heads_tails < 4.99999:
+    print("heads")
+else:
+    print("tails")
+
+# These both do the same thing
+
+random_h_T = random.randint(0, 1)
+print(random_h_T)
+
+if heads_tails == 0:
+    print("Heads")
+else:
+    print("Tails")

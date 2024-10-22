e7c945074bdd980d95d63cd0deda197d53720381
dakota <dakotaabarnes@gmail.com>
2024-10-21 22:47:04 -0500

    2nd option

--- PayBill.py
+++ PayBill.py
@@ -1,6 +1,10 @@
 import random
-
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
-bill = random.choice(friends)
 
+# 1st Option
+bill = random.choice(friends)
 print(f"{bill} is paying the bill tonight")
+
+# 2nd Option
+random_index = random.randint(0, 4)
+print(friends[random_index])

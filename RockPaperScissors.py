7706028c3465caace59e0cd3a3acd539b1775225
dakota <dakotaabarnes@gmail.com>
2024-10-22 00:12:02 -0500

    rockpaperscissors

---
+++ RockPaperScissors.py
@@ -0,0 +1,31 @@
+import random
+
+print("Let's play Rock Paper Scissors together!")
+
+# rock beats scissors
+# scissors beats paper
+# paper beats rock
+
+rps = ["Rock", "Paper", "Scissors"]
+
+player_1 = input("Do you choose Rock, Paper, or Scissors? ").lower()
+ai = random.choice(rps)
+
+print(f"Opponent chose {ai}")
+
+while True:
+
+    if player_1 == ai:
+        print("Tie, try again.")
+    elif player_1 == "rock" and ai == "scissors" or \
+        player_1 == "scissors" and ai == "paper" or \
+        player_1 == "paper" and ai == "rock":
+        print("You Win")
+    else:
+        print("You Lose")
+        exit()
+
+
+
+
+

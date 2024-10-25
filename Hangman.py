ebbfd326a4ac0701f4260c73e1eec55add6bf60b
dakota <dakotaabarnes@gmail.com>
2024-10-24 20:37:23 -0500

    placeholders

--- Hangman.py
+++ Hangman.py
@@ -3,9 +3,14 @@ word_list = ["aardvark", "baboon", "camel"]
 
 rando = random.choice(word_list)
 print(rando)
+
+placeholder = "_"
+for char in rando:
+    if
 choice = input("Guess a letter: ").lower()
 
-if choice in rando:
-    print("right")
-else:
-    print("wrong"
+for letter in rando:
+    if  letter == choice:
+        print("right")
+    else:
+        print("wrong")
